3a002ea9ef028d9ad2436840efb333b4ba6a6e67
FruitsAndVegetables <99060046+FruitsAndVegetables@users.noreply.github.com>
2025-06-26 19:21:04 +0800

    Update home.js

--- my-website/my-website/js/home.js
+++ my-website/my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'e88ec1af5d4d421d69cbd8b48eec757f';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
